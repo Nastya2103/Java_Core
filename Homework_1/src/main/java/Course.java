ef39961dec324bdcb7b303fcfa027a9b5858842e
Nastya2103 <korobeynikova2103@mail.ru>
2022-12-20 19:19:13 +0300

    Практическая работа к уроку 1

---
+++ Homework_1/src/main/java/Course.java
@@ -0,0 +1,41 @@
+public class Course {
+    String result;
+    private int swim;
+    private int run;
+    private int shooting;
+
+    public Course(int swim, int run, int shooting) {
+        this.swim = swim;
+        this.run = run;
+        this.shooting = shooting;
+    }
+
+    public String doIt(Team team) {
+        result = "Команда: " + team.getName() + "." + '\n';
+        for (Teammait teammait: team.getTeammaits()) {
+            result += "Участник команды: " + teammait.getName() + "." + '\n';
+            int value = teammait.getDistance();
+            actionSwim(value);
+            actionRun(value);
+            actionShooting(value);
+        }
+        return result;
+    }
+
+    private void actionShooting(int value) {
+        if (value>=(swim + run + shooting)) result += "Препятствие 'Стрельба' - участник преодолел." + '\n';
+        else result += "Препятствие 'Стрельба' - участник устал после плавания и бега, и не смог преодолеть." + '\n';
+    }
+
+    private void actionRun(int value) {
+        if (value>=(swim + run)) result += "Препятствие 'Пробежать' - участник преодолел." + '\n';
+        else result += "Препятствие 'Пробежать' - участник устал после плавания и не смог преодолеть." + '\n';
+
+    }
+
+    private void actionSwim(int value) {
+        if (value>=swim) result += "Препятствие 'Проплыть' - участник преодолел." + '\n';
+        else result += "Препятствие 'Проплыть' - участник не смог преодолеть." + '\n';
+
+    }
+}
